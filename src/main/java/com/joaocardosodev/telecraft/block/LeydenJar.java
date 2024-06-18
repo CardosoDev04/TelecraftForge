6f49cd03c321fb888a7c21107e1914ad869b0be6
Joao Cardoso <a50465@alunos.isel.pt>
2024-06-19 00:05:17 +0100

    Added inventory to leyden jar

--- src/main/java/com/joaocardosodev/telecraft/block/LeydenJar.java
+++ src/main/java/com/joaocardosodev/telecraft/block/LeydenJar.java
@@ -2,11 +2,14 @@ package com.joaocardosodev.telecraft.block;
 
 import com.joaocardosodev.telecraft.blockentity.LeydenJarBlockEntity;
 import com.joaocardosodev.telecraft.init.ModBlockEntities;
+import com.joaocardosodev.telecraft.util.TickableBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -14,15 +17,22 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.logging.Logger;
 
 public class LeydenJar extends HorizontalDirectionalBlock implements EntityBlock {
 
+
+    private static final Logger LOGGER = Logger.getLogger(LeydenJar.class.getName());
     private static final VoxelShape SHAPE = Block.box(2,0,2,14,14,14);
 
     @Override
@@ -50,12 +60,65 @@ public class LeydenJar extends HorizontalDirectionalBlock implements EntityBlock
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
         if(!level.isClientSide) {
             BlockEntity be = level.getBlockEntity(pos);
+
             if(be instanceof LeydenJarBlockEntity blockEntity) {
-                int counter = blockEntity.incrementCounter();
-                player.sendSystemMessage(Component.literal("LeydenJar has been used %d times".formatted(counter)));
+                ItemStackHandler block_inventory = blockEntity.getInventory();
+
+                ItemStack playerStack = player.getItemInHand(hand);
+
+                if(playerStack.isEmpty()){
+                    if(block_inventory.getStackInSlot(0).isEmpty()) {
+                        player.sendSystemMessage(Component.literal("Theres no item in the jar"));
+                        return InteractionResult.SUCCESS;
+                    }
+
+                    ItemStack extracted_item = block_inventory.extractItem(0,player.isCrouching() ? block_inventory.getSlotLimit(0) : 1 , false);
+
+                    player.setItemInHand(hand,extracted_item);
+
+                } else {
+                    ItemStack to_insert = playerStack.copy();
+
+                    to_insert.setCount(1);
+
+                    ItemStack leftover = block_inventory.insertItem(0,to_insert,false);
+
+                    ItemStack remainder = playerStack.copy();
+
+                    remainder.setCount(remainder.getCount() - 1);
+                    remainder.grow(leftover.getCount());
+                    player.setItemInHand(hand, remainder);
+
+                    return InteractionResult.SUCCESS;
+                }
+
                 return InteractionResult.sidedSuccess(level.isClientSide());
             }
         }
-        return super.use(state,level,pos,player,hand,result);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return TickableBlockEntity.getTickerHelper(level);
+    }
+
+    @Override
+    public void onRemove(BlockState oldState, Level level, BlockPos pos, BlockState state, boolean isMoving) {
+       if(!level.isClientSide) {
+           BlockEntity be = level.getBlockEntity(pos);
+
+           if(be instanceof LeydenJarBlockEntity blockEntity) {
+               ItemStackHandler inventory = blockEntity.getInventory();
+               for(int i = 0; i < inventory.getSlots(); i++) {
+                   ItemStack stack = inventory.getStackInSlot(i);
+                   var entity = new ItemEntity(level,pos.getX() + 0.5,pos.getY() + 0.5,pos.getZ()+ 0.5,stack);
+                   level.addFreshEntity(entity);
+               }
+           }
+       }
+        super.onRemove(state, level, pos, oldState, isMoving);
+
     }
 }
--- src/main/java/com/joaocardosodev/telecraft/blockentity/LeydenJarBlockEntity.java
+++ src/main/java/com/joaocardosodev/telecraft/blockentity/LeydenJarBlockEntity.java
@@ -3,22 +3,38 @@ package com.joaocardosodev.telecraft.blockentity;
 
 import com.joaocardosodev.telecraft.Telecraft;
 import com.joaocardosodev.telecraft.init.ModBlockEntities;
+import com.joaocardosodev.telecraft.util.TickableBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
 
-public class LeydenJarBlockEntity extends BlockEntity {
+public class LeydenJarBlockEntity extends BlockEntity implements TickableBlockEntity {
+    private int ticks;
 
-    int counter;
+    private final ItemStackHandler inventory = new ItemStackHandler(1) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            super.onContentsChanged(slot);
+            LeydenJarBlockEntity.this.setChanged();
+        }
+    };
 
+    private final LazyOptional<ItemStackHandler> optional = LazyOptional.of(() -> this.inventory);
 
     @Override
     public void load(CompoundTag nbt) {
         super.load(nbt);
 
         CompoundTag telecraftModData = nbt.getCompound(Telecraft.MODID);
-        this.counter = telecraftModData.getInt("Counter");
+        this.inventory.deserializeNBT(telecraftModData.getCompound("LeydenJarInventory"));
     }
 
     @Override
@@ -26,7 +42,7 @@ public class LeydenJarBlockEntity extends BlockEntity {
         super.saveAdditional(nbt);
 
         var telecraftModData = new CompoundTag();
-        telecraftModData.putInt("Counter",counter);
+        telecraftModData.put("LeydenJarInventory", this.inventory.serializeNBT());
         nbt.put(Telecraft.MODID,telecraftModData);
     }
 
@@ -34,7 +50,37 @@ public class LeydenJarBlockEntity extends BlockEntity {
         super(ModBlockEntities.LEYDEN_JAR_ENTITY.get(),p_155229_, p_155230_);
     }
 
-    public int incrementCounter() {
-        return ++this.counter;
+
+    @Override
+    public void tick() {
+        if(this.level == null) return;
+
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap) {
+        return cap == ForgeCapabilities.ITEM_HANDLER ? this.optional.cast() : super.getCapability(cap);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        this.optional.invalidate();
+    }
+
+    public ItemStackHandler getInventory() {
+        return this.inventory;
+    }
+
+    public ItemStack getItem() {
+        return this.inventory.getStackInSlot(0);
+    }
+
+    public void setItem(ItemStack stack) {
+        this.inventory.setStackInSlot(0,stack);
+    }
+
+    public LazyOptional<ItemStackHandler> getOptional() {
+        return this.optional;
     }
 }
